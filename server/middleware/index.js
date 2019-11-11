661da8d9e0dea24d1e81419b75679b791fdafcce
Justin Armstrong <jxa1762@rit.edu>
2019-11-10 19:50:55 -0500

    Middleware

---
+++ server/middleware/index.js
@@ -0,0 +1,37 @@
+// - FUNCTIONS -
+// If we aren't logged in, navigate to the default/ main page
+const requiresLogin = (req, res, next) => {
+  if(!req.session.account) {
+    res.redirect('/');
+  }
+  
+  return next();
+};
+
+// If we are logged out, keep us where we need to be
+const requiresLogout = (req, res, next) => {
+  if(!req.session.account) {
+    res.redirect('/inPage');
+  }
+  
+  return next();
+};
+
+const requiresSecure = (req, res, next) => {
+  if(req.headers['x-forwarded-proto'] !== 'https') {
+    return res.redirect(`https://${req.hostname}${req.url}`);
+  }
+  
+  return next();
+};
+
+const bypassSecure = (req, res, next) => next();
+
+// - EXPORTS -
+
+// Environment Variable
+if (process.env.NODE_ENV === 'production') {
+  module.exports.requiresSecure = requiresSecure;
+} else {
+  module.exports.requiresSecure = bypassSecure;
+}
